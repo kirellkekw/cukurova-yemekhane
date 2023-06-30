f2f3ccca65bb339b9041f42da61cfecd848c7a99
arda <arda1033@gmail.com>
2023-06-30 17:52:23 +0300

    refactor: language consistency

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ async def bugün():
     except KeyError:
         return {"error": "No content for today."}
 
+
 @app.get(path="/tomorrow/", description="Get tomorrow's content. Will return error message if there is no content for tomorrow.")
 async def yarın():
     tomorrow = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%d.%m.%Y")
@@ -34,29 +35,31 @@ async def yarın():
     except KeyError:
         return {"error": "No content for tomorrow."}
     
+    
 @app.get(path="/closest/", description="Get closest day's content. Will return content with date included.")
-async def en_yakın():
-
+async def en_yakın_gün():
     closestDate:str = list(calendar.keys())[0]
     data:str = calendar[closestDate]
-    data["tarih"] = closestDate
+    data["date"] = closestDate
     return data
 
 
 @app.get(path="/date/", description="Get chosen day's content with query parameter.")
-async def gün(tarih: str = Query(enum=list(calendar.keys()))):
+async def gün(date: str = Query(enum=list(calendar.keys()))):
     try:
-        return calendar[tarih]
+        return calendar[date]
     except KeyError:
         return {"error": "No content for this date or meal."}
 
 
 @app.get(path="/meal/", description="Get chosen day's chosen meal with query parameters. Will return error message if chosen meal is not available.")
-async def yemek(tarih:str=Query(enum=list(calendar.keys())), yemek:str=Query(enum=["1", "2", "3", "4"])):
+async def yemek(date:str=Query(enum=list(calendar.keys())), meal:str=Query(enum=["1", "2", "3", "4"])):
     try:
-        return calendar[tarih][f"yemek{yemek}"]
+        return calendar[date][f"yemek{meal}"]
     except KeyError:
         return {"error": "No content for this date or meal."}
+    
+
     
 
 # delete comment if you want to get days without query parameters
