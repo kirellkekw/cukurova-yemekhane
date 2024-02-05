ffde10342f948f52385cf07310c1c97ecb79fc77
arda <arda1033@gmail.com>
2024-02-05 17:22:21 +0300

    now food list refreshes every day

--- main.py
+++ main.py
@@ -5,17 +5,44 @@ import jsonpickle
 import uvicorn
 import datetime
 
+import asyncio
+
 r = requests.get(url="https://yemekhane.cu.edu.tr/yemeklistejson.asp")
 r.encoding = "ISO-8859-9"
 r = re.sub(r'<meta .*>', '', r.text)
 
 calendar = jsonpickle.decode(r)
 
+
+async def refresh_food_list():
+    global calendar
+
+    """Purge files older than max_file_age defined in config.py from download_directory"""
+
+    print("File purge subprocess started.")  # debug
+
+    while True:
+        await asyncio.sleep(60*60*24)  # check every day
+        r = requests.get(url="https://yemekhane.cu.edu.tr/yemeklistejson.asp")
+        r.encoding = "ISO-8859-9"
+        r = re.sub(r'<meta .*>', '', r.text)
+
+        calendar = jsonpickle.decode(r)
+
+
 app = FastAPI()
+
+
+@app.on_event("startup")  # run this function when the server starts
+async def startup_event():
+    """Creates sub-processes to run in the background when the server starts."""
+    asyncio.create_task(refresh_food_list())
+
 
 @app.get(path="/", description="Get root tree of the API.")
 async def root():
     return calendar
+
 
 @app.get(path="/day/{day}")
 async def day(day: str):
@@ -23,6 +50,7 @@ async def day(day: str):
         return calendar[day]
     except KeyError:
         return {"error": "No content for this date or meal."}
+
 
 @app.get(path="/today", description="Get today's content. Will return error message if there is no content for today.")
 async def today():
@@ -32,20 +60,23 @@ async def today():
     except KeyError:
         return {"error": "No content for today."}
 
+
 @app.get(path="/tomorrow", description="Get tomorrow's content. Will return error message if there is no content for tomorrow.")
 async def tomorrow():
-    tomorrow = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%d.%m.%Y")
+    tomorrow = (datetime.date.today() +
+                datetime.timedelta(days=1)).strftime("%d.%m.%Y")
     try:
         return calendar[tomorrow]
     except KeyError:
-        return {"error": "No content for tomorrow."} 
+        return {"error": "No content for tomorrow."}
+
 
 @app.get(path="/closest", description="Get closest day's content. Will return content with date included.")
 async def closest_day():
-    closestDay:str = list(calendar.keys())[0]
-    data:str = calendar[closestDay]
+    closestDay: str = list(calendar.keys())[0]
+    data: str = calendar[closestDay]
     data["date"] = closestDay
     return data
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=2000)
+    uvicorn.run("main:app", host="0.0.0.0", port=2000, loop="asyncio")
